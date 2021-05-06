7a400dd1bf611b6f4d45d3c746f793c141f0f494
Malte Petersen <mp@marketoolz.com>
2021-05-06 23:25:36 +0200

    ASMWrapperServiceTest erweitert

--- src/test/java/de/fh/kiel/advancedjava/pojomodel/service/ASMWrapperServiceTest.java
+++ src/test/java/de/fh/kiel/advancedjava/pojomodel/service/ASMWrapperServiceTest.java
@@ -1,10 +1,12 @@
 package de.fh.kiel.advancedjava.pojomodel.service;
 
 import de.fh.kiel.advancedjava.pojomodel.TestingUtil;
+import de.fh.kiel.advancedjava.pojomodel.exception.PojoAlreadyExists;
 import de.fh.kiel.advancedjava.pojomodel.model.AttributeInfo;
+import de.fh.kiel.advancedjava.pojomodel.model.Pojo;
 import de.fh.kiel.advancedjava.pojomodel.model.PojoInfo;
 import de.fh.kiel.advancedjava.pojomodel.repository.PojoRepository;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,11 +17,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
@@ -33,26 +34,28 @@ public class ASMWrapperServiceTest {
     private TestingUtil testingUtil;
 
 
-    @BeforeEach
-    void init(@Mock PojoRepository pojoRepository) {
+
+    @Test
+    void readPojoByByteCode(@Mock PojoRepository pojoRepository) throws IOException {
         asmWrapperService = new ASMWrapperService(pojoRepository);
 
         Mockito.when(pojoRepository.findById(any(String.class))).thenReturn(Optional.empty());
-
-
-    }
-    public static byte[] loadData(String location) throws IOException {
-        return Files.readAllBytes(Paths.get(location));
-    }
-    @Test
-    void readPojoByByteCode() throws IOException {
-
         var attributes =  new HashSet<>(Set.of(new AttributeInfo("name", "java.lang.String", "private", "String", "java.lang"),new AttributeInfo("id", "java.lang.Long", "private", "Long", "java.lang")));
         var expected = new PojoInfo("de.fh.kiel.advancedjava.pojomodel.exampleData.DefaultClass","DefaultClass", "de.fh.kiel.advancedjava.pojomodel.exampleData", "java.lang.Object","Object","java.lang",
                attributes,Set.of());
 
         var actual =   asmWrapperService.read(testingUtil.getClassValue("DefaultClass"));
         assertEquals(expected, actual);
+
+    }
+    @Test
+    void throwPojoAlreadyExists(@Mock PojoRepository pojoRepository) throws IOException {
+        asmWrapperService = new ASMWrapperService(pojoRepository);
+        Mockito.when(pojoRepository.findById(any(String.class))).thenReturn(Optional.of(Pojo.builder().completePath("de.fh.kiel.advancedjava.pojomodel.exampleData.DefaultClass").build()));
+
+
+      assertThrows(PojoAlreadyExists.class,() -> asmWrapperService.read(testingUtil.getClassValue("DefaultClass")));
+
 
     }
 }
