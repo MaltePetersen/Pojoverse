95e0ad2f0f4ad456ce5ae7350432d452f3fd7858
Malte Petersen <mp@marketoolz.com>
2021-06-12 14:00:04 +0200

    swagger

--- src/main/java/de/fh/kiel/advancedjava/pojomodel/PojoApplication.java
+++ src/main/java/de/fh/kiel/advancedjava/pojomodel/PojoApplication.java
@@ -1,5 +1,6 @@
 package de.fh.kiel.advancedjava.pojomodel;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -9,11 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(info = @Info(
         title = "Pojo",
-        version = "0.0",
-        description = "A project Java course FH Kiel",
+        version = "1.0",
+        description = """
+               This application is able to store, update, delete, visualize simple POJO's (Plain old Java objects).
+               The pojos are modeled, stored and visualized in a neo4j graph database.
+               You can login into the db here: http://localhost:7474/browser/
+               A user can interact with the system through this REST-APi.                                
+                """ ,
         license = @License(name = "Apache 2.0", url = "https://github.com/MaltePetersen"),
-        contact = @Contact(url = "https://github.com/MaltePetersen", name = "Malte", email = "malte.petersen11@gmail.com")
-))
+        contact = @Contact(url = "https://github.com/MaltePetersen", name = "Malte Petersen", email = "malte.petersen11@gmail.com")
+), externalDocs = @ExternalDocumentation(description = "Gitlab", url = "https://gitlab.iue.fh-kiel.de/advanced-java-2021/pojo-malte"))
 @SpringBootApplication
 public class PojoApplication {
 
